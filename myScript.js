ed13b4c88363d9bcfc0d9467637551a6efa2ea06
dragsec <draganyes@gmail.com>
2021-05-26 08:56:25 +0200

    not bad

--- myScript.js
+++ myScript.js
@@ -30,3 +30,88 @@ var app = new Vue ({
             ],
         },
 })
+
+var app2 = new Vue ({
+    el: "#app2",
+        data : {
+            SupportArray:[
+                
+                {
+                text: "> Privacy",
+                url: "#"
+                },
+
+                {
+                    text:"> Terms",
+                    url: "#"
+                },
+
+                {
+                    text:"> Cookie Policy",
+                    url:"#",
+                },
+
+                {
+                    text:"> Sitemap",
+                    url:"#",
+                },
+
+            ],
+
+            ExploreArray:[
+                {
+                    text: "> Accomodation",
+                    url: "#"
+                },
+
+                {
+                    text: "> Arc Academics",
+                    url: "#"
+                },
+
+                {
+                    text: "> Central Services",
+                    url: "#"
+                },
+
+                {
+                    text: "> Conferences and Events",
+                    url: "#"
+                },
+
+                {
+                    text: "> Food and Drink",
+                    url: "#"
+                },
+ 
+            ],
+
+            VisitArray:[
+                {
+                    text: "> Admission",
+                    url: "#"
+                },
+
+                {
+                    text: "> Contact Us",
+                    url: "#"
+                },
+
+                {
+                    text: "> FAQs",
+                    url: "#"
+                },
+
+                {
+                    text: "> Request Information",
+                    url: "#"
+                },
+
+                {
+                    text: "> Privacy Notice",
+                    url: "#"
+                },
+
+            ],
+        },
+})
