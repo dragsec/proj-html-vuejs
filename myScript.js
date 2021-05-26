6cf8327e6d813a7a10321a86e6153a2999835879
dragsec <draganyes@gmail.com>
2021-05-26 08:38:47 +0200

    vue

---
+++ myScript.js
@@ -0,0 +1,32 @@
+var app = new Vue ({
+    el: "#app",
+        data : {
+            LinkArray:[
+                
+                {
+                text: "Home",
+                url: "#"
+                },
+
+                {
+                    text:"About",
+                    url: "#"
+                },
+
+                {
+                    text:"Academics",
+                    url:"#",
+                },
+
+                {
+                    text:"Courses",
+                    url:"#",
+                },
+
+                {
+                    text:"News",
+                    url:"#",
+                },
+            ],
+        },
+})
